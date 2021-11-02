59492f6924e23a28a4f3bc526b4e602b7eecbfb9
Astroth1984 <elmounir.othmane666@gmail.com>
2021-11-02 21:31:42 +0100

    add products object

---
+++ src/data.js
@@ -0,0 +1,31 @@
+export const products = [{
+        id: 1,
+        img: "https://ecommerce-platforms.com/wp-content/uploads/2020/06/wix-website-builder.jpg",
+        link: "http://lama.dev",
+    },
+    {
+        id: 2,
+        img: "https://static.jobdone.net/p_sp/images_staging/4322/make-beautiful-wix-website-HRDpp1k7py.png",
+        link: "http://lama.dev",
+    },
+    {
+        id: 3,
+        img: "https://img.bfmtv.com/c/0/708/b031e/0c0ffc89a7ff7f776e03675cf9a.png",
+        link: "http://lama.dev",
+    },
+    {
+        id: 4,
+        img: "",
+        link: "http://lama.dev",
+    },
+    {
+        id: 5,
+        img: "",
+        link: "http://lama.dev",
+    },
+    {
+        id: 6,
+        img: "",
+        link: "http://lama.dev",
+    },
+];
