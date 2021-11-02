b0bd39115ce2b744c9feeabdb79ed5eb1e21bcec
Astroth1984 <elmounir.othmane666@gmail.com>
2021-11-02 01:15:30 +0100

    Portfolio List Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import About from "./components/about/about";
 import Intro from "./components/intro/intro";
+import ProductList from "./components/productList/ProductList";
 
 const App = () => {
   return (
       <div>
         <Intro />
         <About />
+        <ProductList />
       </div>
   );
 };
---
+++ src/components/product/Product.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./product.css";
+
+const Product = () => {
+    return (
+        <div>
+            card
+        </div>
+    )
+}
+
+export default Product
