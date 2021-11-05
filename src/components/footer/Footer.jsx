0694ac9fbea1231f9f61f5698c95682b5ca8da87
Astroth1984 <elmounir.othmane666@gmail.com>
2021-11-05 02:22:04 +0100

    connect social icons to sources

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { ThemeContext } from '../../context';
+import { useContext } from 'react';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+    faLinkedin,
+    faGithub,
+    faStackOverflow,
+    faCodepen
+  } from "@fortawesome/free-brands-svg-icons";
+import "./footer.css";
+
+const Footer = () => {
+
+    const theme = useContext(ThemeContext);
+    const darkMode = theme.state.darkMode;
+    return (
+        <div class="social-container" style={{backgroundColor: darkMode && "#666"}}>
+            <div className="social-bg"></div>
+            <h3>Social Follow</h3>
+            <a href="https://www.linkedin.com/in/el-mounir-othmane-21ab46147/" target="_blank" rel="noreferrer"
+               className="social">
+               <FontAwesomeIcon icon={faLinkedin} size="2x" />
+            </a>
+            <a href="https://github.com/Astroth1984" target="_blank" rel="noreferrer"
+               className="social">
+               <FontAwesomeIcon icon={faGithub} size="2x" />
+            </a>
+            <a href="https://stackoverflow.com/users/10167565/elmounir-othmane" target="_blank" rel="noreferrer" className="social">
+                <FontAwesomeIcon icon={faStackOverflow} size="2x" />
+            </a>
+            <a href="https://codepen.io/astroth1984" target="_blank" rel="noreferrer"
+                className="social">
+                <FontAwesomeIcon icon={faCodepen} size="2x" />
+            </a>
+        </div>
+        
+    )
+}
+
+export default Footer
