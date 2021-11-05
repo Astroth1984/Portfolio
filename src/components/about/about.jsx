1ca83c9299590ba76b7d69161464474a043da271
Astroth1984 <elmounir.othmane666@gmail.com>
2021-11-05 02:18:10 +0100

    Letterboxd social Network

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -8,35 +8,37 @@ const About = () => {
             <div className="a-left">
                 <div className="a-card bg"></div>
                 <div className="a-card">
-                    <img src="https://images.pexels.com/photos/3585047/pexels-photo-3585047.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" 
+                    <img src="https://imgsrc.cineserie.com/2021/02/lunatic-at-large-un-projet-abandonne-de-stanley-kubrick-va-se-tourner.jpg?ver=1" 
                          alt="" 
                          className="a-img" 
                     />
                 </div>
             </div>
             <div className="a-right">
-            <h1 className="a-title">About Me</h1>
-        <p className="a-sub">
-          It is a long established fact that a reader will be distracted by the
-          readable content.
-        </p>
-        <p className="a-desc">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat duis aute irure dolor in reprehende.
-        </p>
-        <div className="a-award">
-          <img src={Award} alt="" className="a-award-img" />
-          <div className="a-award-texts">
-            <h4 className="a-award-title">International Design Awards 2021</h4>
-            <p className="a-award-desc">
-              Nemo enim ipsam voluptatem quia voluptas sit aspernatur autodit
-              and fugit.
-            </p>
-          </div>
-        </div>
+                <h1 className="a-title">About Me</h1>
+                <p className="a-sub">
+                  It is a long established fact that a reader will be distracted by the
+                  readable content.
+                </p>
+                <p className="a-desc">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                  eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                  minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                  aliquip ex ea commodo consequat duis aute irure dolor in reprehende.
+                </p>
+              <div className="a-award">
+                <a href="https://letterboxd.com/astroth1984/" target="_blank" rel="noreferrer">
+                  <img src={Award} alt="" className="a-award-img" />
+                </a>
+              <div className="a-award-texts">
+                <h4 className="a-award-title">International Design Awards 2021</h4>
+                <p className="a-award-desc">
+                  Nemo enim ipsam voluptatem quia voluptas sit aspernatur autodit
+                  and fugit.
+                </p>
+              </div>
             </div>
+          </div>
         </div>
     )
 }
