d5773f650ac45ebfe37be4c54f0daccce71e0c0c
Astroth1984 <elmounir.othmane666@gmail.com>
2021-11-09 01:46:51 +0100

    useEffect on played song & skipSong hook

--- src/components/mp3player/components/Player.jsx
+++ src/components/mp3player/components/Player.jsx
@@ -1,15 +1,56 @@
-import React from 'react';
+import React, {useState, useRef, useEffect} from 'react';
 import PlayerDetails from './PlayerDetails';
 import PlayerControls from './PlayerControls';
 
 const Player = (props) => {
+    const audioEl = useRef(null);
+    const [isPlaying, setIsPlaying] = useState(false);
+
+    useEffect(() => {
+        if(isPlaying){
+            audioEl.current.play()
+        } else {
+            audioEl.current.pause()
+        }
+    });
+
+    const SkipSong = (forwards = true) => {
+        if (forwards) {
+            props.setCurrentSongIndex(() => {
+                let temp = props.currentSongIndex;
+                temp++;
+
+                if (temp > props.songs.length - 1) {
+                    temp = 0;
+                }
+
+                return temp;
+            });
+        } else {
+            props.setCurrentSongIndex(() => {
+                let temp = props.currentSongIndex;
+                temp--;
+
+                if (temp < 0) {
+                    temp = props.songs.length - 1;
+                }
+
+                return temp;
+            });
+        }
+    }
+
     return (
         <div className="c-player">
-            <audio></audio>
+            <audio src={props.songs[props.currentSongIndex].src} ref={audioEl}></audio>
             <h4>Playing now</h4>
             <PlayerDetails song={props.songs[props.currentSongIndex]} />
-            <PlayerControls /> 
-            <p>Next up: <span><strong>{props.songs[props.nextSongIndex].title}</strong> by <strong>{props.songs[props.nextSongIndex].artist}</strong></span></p>
+            <PlayerControls 
+                isPlaying={isPlaying} 
+                setIsPlaying={setIsPlaying} 
+                SkipSong={SkipSong} 
+            /> 
+            <p>Next up: <span><strong>{props.songs[props.nextSongIndex]?.title}</strong> by <strong>{props.songs[props.nextSongIndex]?.artist}</strong></span></p>
         </div>
     )
 }
