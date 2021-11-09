5af74ff7085ec2be54bd93d3749f1d1edc43e313
Astroth1984 <elmounir.othmane666@gmail.com>
2021-11-09 01:47:44 +0100

    onClick on skip & play buttonsé

--- src/components/mp3player/components/PlayerControls.jsx
+++ src/components/mp3player/components/PlayerControls.jsx
@@ -4,16 +4,19 @@ import { faPlay, faPause, faForward, faBackward } from '@fortawesome/free-solid-
 
 
 
-function PlayerControls() {
+function PlayerControls(props) {
     return (
         <div className="c-player--controls">
-            <button className="skip-btn">
+            <button className="skip-btn" onClick={() => props.SkipSong(false)}>
                 <FontAwesomeIcon icon={faBackward} />
             </button>
-            <button className="play-btn">
-                <FontAwesomeIcon icon={faPlay} />
+            <button 
+                className="play-btn" 
+                onClick={() => props.setIsPlaying(!props.isPlaying)}
+            >
+                <FontAwesomeIcon icon={props.isPlaying ? faPause : faPlay} />
             </button>
-            <button className="skip-btn">
+            <button className="skip-btn" onClick={() => props.SkipSong()}>
                 <FontAwesomeIcon icon={faForward} />
             </button>
         </div>
