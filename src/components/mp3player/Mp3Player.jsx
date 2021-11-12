f89fc2b22c133417cdd5f4c35a863036634a65ef
Astroth1984 <elmounir.othmane666@gmail.com>
2021-11-12 14:51:10 +0100

    set new songs Object Array

--- src/components/mp3player/Mp3Player.jsx
+++ src/components/mp3player/Mp3Player.jsx
@@ -20,22 +20,58 @@ const Mp3Player = () => {
             src: "./music/nutshell.mp3"
           },
           {
-            title: "Song 3",
-            artist: "Artist 3",
-            img_src: "./images/song-2.jpg",
-            src: "./music/in-this-river.mp3"
+            title: "Hunger",
+            artist: "Temple Of Dog",
+            img_src: "./images/song-3.jpg",
+            src: "./music/ihunger-strike.mp3"
           },
           {
-            title: "Song 4",
-            artist: "Artist 4",
-            img_src: "./images/song-1.jpg",
-            src: "./music/nutshell.mp3"
+            title: "Slip Away",
+            artist: "Mad Season",
+            img_src: "./images/song-4.jpg",
+            src: "./music/slip-away.mp3"
+          },
+          {
+            title: "Society",
+            artist: "Eddie Vedder",
+            img_src: "./images/song-5.jpg",
+            src: "./music/society.mp3"
+          },
+          {
+            title: "The Gift",
+            artist: "Seether",
+            img_src: "./images/song-6.jpg",
+            src: "./music/the-gift.mp3"
+          },
+          {
+            title: "Falls Apart",
+            artist: "Hurt",
+            img_src: "./images/song-7.jpg",
+            src: "./music/falls-apart.mp3"
+          },
+          {
+            title: "To Remind You",
+            artist: "Staind",
+            img_src: "./images/song-8.jpg",
+            src: "./music/remind.mp3"
+          },
+          {
+            title: "J'accuse",
+            artist: "Damien Saez",
+            img_src: "./images/song-9.jpg",
+            src: "./music/jaccuse.mp3"
+          },
+          {
+            title: "Rock Out",
+            artist: "Motorhead",
+            img_src: "./images/song-10.jpg",
+            src: "./music/rock-out.mp3"
           }
     ]);
 
     const [currentSongIndex, setCurrentSongIndex] = useState(0);
     const [nextSongIndex, setNexttSongIndex] = useState(currentSongIndex + 1);
-    const [toggle, setToggle] = useState(false);
+    const [toggle, setToggle] = useState(true);
 
     const handleToggle = () => {
         setToggle(!toggle)
